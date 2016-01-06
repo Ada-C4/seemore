a8ffc55fb6f7bcdce21821deab8be5023dc2615c
Annalee <annaleeherrera@gmail.com>
2016-01-06 15:28:51 -0800

    validations and models

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,2 +1,6 @@
 class Medium < ActiveRecord::Base
+  belongs_to :mark
+
+  validates :mark_id, :date_posted, :link, :medium_type, presence: true
+  
 end
--- app/models/spy.rb
+++ app/models/spy.rb
@@ -1,2 +1,7 @@
 class Spy < ActiveRecord::Base
+  has_and_belongs_to_many :marks
+  has_many :media, through: :marks
+
+  validates :uid, :username, :provider, presence: true
+  validates :uid, :username, uniqueness: true
 end
