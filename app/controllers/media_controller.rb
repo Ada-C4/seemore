cef783d25ea7de1a268d60b382c6f1b820b411c2
Logan McDonald <loganmcdona11@gmail.com>
2016-01-06 15:46:35 -0800

    Added views we want.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,2 +1,4 @@
 class MediaController < ApplicationController
+  def index
+  end
 end
--- spec/models/spy_spec.rb
+++ spec/models/spy_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Spy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
