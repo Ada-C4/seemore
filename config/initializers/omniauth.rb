b0f3bc73709a6411da218ea8fa848a4a6f01211a
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-05 18:29:41 -0800

    Commenting out omniauth config for now just in case. Creating .env file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 # config/initializers/omniauth.rb
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, YOUR_CLIENT_ID, YOUR_CLIENT_SECRET
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"]
+# end
