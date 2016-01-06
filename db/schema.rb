31d39dd65df31616d3cf4093cf0f9a7efd36204e
noglows <jessica.noglows@outlook.com>
2016-01-06 14:35:49 -0800

    Added a subscription model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106222413) do
+ActiveRecord::Schema.define(version: 20160106223211) do
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string   "username"
+    t.string   "uid"
+    t.string   "provider"
+    t.string   "avatar_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "uid"
