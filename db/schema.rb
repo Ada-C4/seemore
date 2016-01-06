cc22cbb3b438713033e36623ffef7033ee558941
noglows <jessica.noglows@outlook.com>
2016-01-06 14:42:16 -0800

    Created the Story model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106223211) do
+ActiveRecord::Schema.define(version: 20160106224158) do
+
+  create_table "stories", force: :cascade do |t|
+    t.string   "uid"
+    t.string   "text"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subscriptions", force: :cascade do |t|
     t.string   "username"
