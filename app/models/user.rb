f18d8394e9c29380f4615db0740cf8ced2291d59
Rebecca Tolmach <rmtolmach@gmail.com>
2016-01-06 16:52:14 -0800

    Adding relationship and class method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,27 @@
 class User < ActiveRecord::Base
+  has_and_belongs_to_many :authors
+
   validates :name, presence: true
   validates :uid, presence:true, uniqueness: true
   validates :provider, presence: true
-  
 
-
+  def self.find_or_create_from_omniauth(auth_hash)
+    user = self.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+    if !user.nil?
+      #user found, return user and continue on with your life
+      return user
+    else
+      # create a new user.
+      user = User.new
+      user.uid = auth_hash["uid"]
+      user.provider = auth_hash["provider"]
+      user.name = auth_hash["info"]["name"]
+      user.image = auth_hash["info"]["image"]
+      if user.save
+        return user
+      else
+        return nil
+      end
+    end
+  end
 end
