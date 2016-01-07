6bba3f8d7dc7d0763571d31eb10101400239597b
Rebecca Tolmach <rmtolmach@gmail.com>
2016-01-06 17:00:25 -0800

    Tests for omniauth mock are passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,7 +52,21 @@ RSpec.describe User, type: :model do
       user.image = nil
       expect(user).to be_valid
     end
+  end
 
+  describe ".initialize_from_omniauth" do
+    let(:user) { User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:twitter]) }
+
+    it "creates a valid user" do
+      expect(user).to be_valid
+    end
+
+    context "when it's invalid" do
+      it "returns nil" do
+        user = User.find_or_create_from_omniauth({"uid" => "123", "info" => {}})
+        expect(user).to be_nil
+      end
+    end
   end
 
 end
