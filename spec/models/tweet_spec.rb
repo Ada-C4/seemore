f0b23448b7a8d266a2c5157117ab650e89b89d8f
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-07 11:28:12 -0800

    added specs for Tweet model

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tweet) { Tweet.new(
+    twitter_id:      "12345678",
+    text: "This is a tweet",
+    uri: "https://twitter.com/kdefliese/status/684970660116873216")
+  }
+
+  describe "model validations" do
+    it "is valid" do
+      expect(tweet).to be_valid
+    end
+
+    it "requires a twitter_id" do
+      tweet.twitter_id = nil
+      expect(tweet).to be_invalid
+    end
+
+    it "requires a text field" do
+      tweet.text = nil
+      expect(tweet).to be_invalid
+    end
+
+    it "requires a uri" do
+      tweet.uri = nil
+      expect(tweet).to be_invalid
+    end
+  end
 end
