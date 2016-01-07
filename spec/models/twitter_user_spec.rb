59add9824a58991fe71fb4c7b29ffdaa2e1bb065
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-07 11:04:22 -0800

    created model validation specs for TwitterUser

--- spec/models/twitter_user_spec.rb
+++ spec/models/twitter_user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe TwitterUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:twitter_user) { TwitterUser.new(
+    twitter_id:      "12345678",
+    screen_name: "kdefliese",
+    name: "Katherine Defliese",
+    description: "I really like cats",
+    location: "Seattle, WA",
+    uri: "https://twitter.com/kdefliese",
+    profile_image_uri: "http://pbs.twimg.com/profile_images/672319002706706432/_MQlTm-A_normal.jpg")
+  }
+
+  describe "model validations" do
+    it "is valid" do
+      expect(twitter_user).to be_valid
+    end
+
+    it "requires a twitter_id" do
+      user.name = nil
+      expect(user).to be_invalid
+    end
+
+    it "requires a screen_name" do
+      user.uid = nil
+      expect(user).to be_invalid
+    end
+
+    it "requires a uri" do
+      user.uid = nil
+      expect(user).to be_invalid
+    end
+  end
+
 end
