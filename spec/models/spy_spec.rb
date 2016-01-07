c406abfcac22a28d238bf89d4bfcc8d186cac0a4
Amy Hunter <amyrhunter@gmail.com>
2016-01-06 16:58:07 -0800

    Add validation tests to spy model

--- spec/models/spy_spec.rb
+++ spec/models/spy_spec.rb
@@ -1,4 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Spy, type: :model do
+  before(:each) do
+    Spy.create(
+    uid: "string thing",
+    username: "usery name",
+    image_url: "http://www.imageyimage.com",
+    provider: "twitter")
+  end
+
+  describe "validations" do
+    it "is valid" do
+      expect(spy).to be_valid
+    end
+
+    it "requires a username" do
+      spy.username = nil
+      expect(spy).to be_invalid
+    end
+
+    it "requires a provider" do
+      spy.provider = nil
+      expect(spy).to be_invalid
+    end
+
+    it "requires a uid" do
+      spy.uid = nil
+      expect(spy).to be_invalid
+    end
+
+    it "has a unique uid" do
+      expect(Spy.new(
+        uid: "string thing",
+        username: "usery name",
+        image_url: "http://www.imageyimage.com",
+        provider: "twitter")).to_not be_valid
+    end
+
+    it "has a unique username" do
+      expect(Spy.new(
+        uid: "string thing",
+        username: "usery name",
+        image_url: "http://www.imageyimage.com",
+        provider: "twitter")).to_not be_valid
+    end
+  end
 end
