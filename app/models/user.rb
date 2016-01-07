4a7d451b168392a81fc21cd5ceb52e996a47e4d8
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-06 16:28:02 -0800

    Adding validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :uid, presence:true, uniqueness: true
+  validates :provider, presence: true
+  
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(
+    uid:      "123",
+    provider: "twitter",
+    name: "Beagle",
+    image: "http://wiki.hydrogenaud.io/images/2/2a/Beagle_50x50.gif")
+  }
+
+  describe "validations" do
+    it "is valid" do
+      expect(user).to be_valid
+    end
+
+    it "requires a name" do
+      user.name = nil
+      expect(user).to be_invalid
+    end
+
+    it "requires a uid" do
+      user.uid = nil
+      expect(user).to be_invalid
+    end
+
+    it "requires a provider" do
+      user.provider = nil
+      expect(user).to be_invalid
+    end
+
+    it "does not require an image" do
+      user.image = nil
+      expect(user).to be_valid
+    end
+
+  end
+
 end
