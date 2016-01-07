5184e57870f8837403e90f0c8ede0bf9c16ce253
Meighan Rasley <meighanr@gmail.com>
2016-01-07 14:10:35 -0800

    Only logged in users see logout button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-
+  before_action :current_user
   
+  def current_user
+    if session[:user_id] && !!User.exists?(session[:user_id])
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
 
 end
