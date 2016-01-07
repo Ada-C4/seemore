54e31d97ab90808d511559d6eaf84f1f64dc6a63
Brittany <BrittanyKohler@gmail.com>
2016-01-07 09:39:53 -0800

    Adding rspec tests for session create method in sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe "GET #create" do
+    context "when using developer authentication" do
+      context "is successful" do
+        before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:developer] }
 
+        it "redirects to the home page" do
+          get :create, provider: :developer
+          expect(response).to redirect_to root_path
+        end
+      end
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,15 +9,30 @@ end
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  
+
   config.expect_with :rspec do |expectations|
-   
+
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
-  
+
     mocks.verify_partial_doubles = true
   end
 
+  config.before(:suite) do
+    # Once you have enabled test mode, all requests
+    # to OmniAuth will be short circuited
+    # to use the mock authentication hash.
+    # A request to /auth/provider will redirect
+    # immediately to /auth/provider/callback.
+
+    OmniAuth.config.test_mode = true
+
+    # The mock_auth configuration allows you to
+    # set per-provider (or default) authentication
+    # hashes to return during testing.
+
+    OmniAuth.config.mock_auth[:developer] = OmniAuth::AuthHash.new({:provider => 'developer', :uid => '123545', info: {username: "Brittany"}})
+  end
 end
