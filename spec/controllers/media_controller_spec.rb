4911e1b501c81452f1a845b10ffb54a23c9c455e
Brittany <BrittanyKohler@gmail.com>
2016-01-07 09:58:58 -0800

    Adding rpsec test for media index in media controller

--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe MediaController, type: :controller do
-
+  describe "GET #index" do
+    it "renders the index page" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 end
