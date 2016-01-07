c3e0bc483f73d08ae1e9903b3a7a6b3201598d00
noglows <jessica.noglows@outlook.com>
2016-01-06 16:57:46 -0800

    Initial Twitter setup for pulling tweets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  def twitter
+    Seemore::Application.config.twitter
+  end
+
   def show
+    Seemore::Application.config.twitter
+    @sample_stories = twitter.user_timeline("Schwarzenegger")
   end
 end
