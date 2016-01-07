c3caad6848a0ddc658c8167611de00b941293c67
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-07 10:57:58 -0800

    created TwitterUser model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107001734) do
+ActiveRecord::Schema.define(version: 20160107185658) do
+
+  create_table "twitter_users", force: :cascade do |t|
+    t.integer  "twitter_id"
+    t.string   "screen_name"
+    t.string   "name"
+    t.string   "description"
+    t.string   "location"
+    t.string   "uri"
+    t.string   "profile_image_uri"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "uid"
