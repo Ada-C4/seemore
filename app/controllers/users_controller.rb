fa1fd61c43572600cef250799341a9ae621ed62c
noglows <jessica.noglows@outlook.com>
2016-01-07 12:16:42 -0800

    Fixed sample stories issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    #@sample_stories = twitter.user_timeline("Schwarzenegger")
+    @sample_stories = twitter.user_timeline("Schwarzenegger")
     @vimeo_stories = Vimeo::Simple::User.videos("15397797")
 
   end
