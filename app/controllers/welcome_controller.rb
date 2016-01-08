e2ce82261f8b6fa695b6eb4453b12682241a105a
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-07 16:44:20 -0800

    added spec for Twitter user search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,6 @@ class WelcomeController < ApplicationController
 
   def index
     @twitter_user = TwitterUser.new()
-    @me = $client.user("kdefliese")
-    @you = $client.user("DreyDavis")
     @instagram = Instagram.user_recent_media("1496836825", {:count => 3})
     @instagram_search = Instagram.user_search('dreedles')
     render :index
