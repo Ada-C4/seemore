53f0f13293a1b087d4dff44ad6b1ca73aebac5fb
Logan McDonald <loganmcdona11@gmail.com>
2016-01-08 09:50:49 -0800

    login and logout now working from home

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
+  def new
+    provider = params[:format]
+    redirect_to "/auth/#{provider}"
+  end
+
   def create
     auth_hash = request.env['omniauth.auth']
     @spy = Spy.find_or_create_from_omniauth(auth_hash)
     session[:spy_id] = @spy.id
     redirect_to root_path
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "media#index"
 
+  get '/login' => 'sessions#new'
+  get '/logout' => 'sessions#destroy'
+
   get "marks/search/:provider", to: "marks#search", as: "search"
 
   get "marks/search/:provider/results", to: "marks#results", as: "results"
