523b6a6e07b16ff14464f447bca89cc6f1961601
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-07 16:47:59 -0800

    Adding validations and rspec for InstagramUser model

--- app/models/instagram_user.rb
+++ app/models/instagram_user.rb
@@ -1,2 +1,6 @@
 class InstagramUser < ActiveRecord::Base
+  has_and_belongs_to_many :users
+  validates :instagram_id, presence: true
+  validates :screen_name, presence: true
+  validates :uri, presence: true
 end
--- spec/models/instagram_user_spec.rb
+++ spec/models/instagram_user_spec.rb
@@ -1,4 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe InstagramUser, type: :model do
+  let(:instagram_user) { InstagramUser.new(
+    instagram_id:      "123",
+    screen_name: "zeldapug",
+    name: "Zelda",
+    description: "Love love love I'm a pug",
+    uri: "https://www.instagram.com/dreedles/",
+    profile_image_uri: "https://scontent-sea1-1.cdninstagram.com/hphotos-frc/t51.2885-19/10598334_772391902818043_627378339_a.jpg")
+  }
+
+  describe "model validations" do
+    it "is valid" do
+      expect(instagram_user).to be_valid
+    end
+
+    it "requires an instagram_id" do
+      instagram_user.instagram_id = nil
+      expect(instagram_user).to be_invalid
+    end
+
+    it "requires a screen_name" do
+      instagram_user.screen_name = nil
+      expect(instagram_user).to be_invalid
+    end
+
+    it "requires a uri" do
+      instagram_user.uri = nil
+      expect(instagram_user).to be_invalid
+    end
+  end
+
 end
