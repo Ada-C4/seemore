ac94cc8b7240ea05b75a05dbc33ddf12862cd5fb
Brittany <BrittanyKohler@gmail.com>
2016-01-08 09:56:07 -0800

    Adding first test for marks controller

--- spec/controllers/marks_controller_spec.rb
+++ spec/controllers/marks_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe MarksController, type: :controller do
+  describe "GET #search" do
+    context "twitter" do
+      it "renders the search page" do
+        get :search
+        expect(response).to render_template :search
+      end
+    end
+  end
+
+  describe "GET #index" do
+    it "renders the index page" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 
 end
