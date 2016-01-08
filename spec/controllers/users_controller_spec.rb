150089957a9ca7cca83067dc7b0ed33a9e6b1ad0
noglows <jessica.noglows@outlook.com>
2016-01-08 09:50:07 -0800

    User Controller test coverage

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET 'show'" do
+    it "renders the show view" do
+      get :show
+      expect(subject).to render_template :show
+    end
+  end
 
+describe "GET 'twitter_search'" do
+    let(:params) do
+    {
+      search: "justinbieber"
+    }
+  end
+
+  it "renders the twitter_search view" do
+    get :twitter_search, params
+    expect(subject).to render_template :twitter_search
+  end
+end
+
+describe "GET 'twitter_search_user'" do
+  let(:params) do
+    {
+      id: "justinbieber"
+    }
+  end
+
+  it "renders the twitter_search_user view" do
+    get :twitter_search_user, params
+    expect(subject).to render_template :twitter_search_user
+  end
+
+end
 end
