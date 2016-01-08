2f0c76213bce33b2dd2e0ff82170dd86a4999efa
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-07 16:32:12 -0800

    added search method for Twitter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
+    # will need to add conditional logic for Twitter or Instagram
+    search_term = params[:search_term]
+    @results = $client.user_search(search_term)
   end
 end
