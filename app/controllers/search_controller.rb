4980bcb1a30aab0a8c04c15e03c53830d7310921
Rebecca Tolmach <rmtolmach@gmail.com>
2016-01-08 09:54:49 -0800

    searching working for Twitter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
   def index
     # will need to add conditional logic for Twitter or Instagram
-    search_term = params[:search_term]
-    @results = $client.user_search(search_term)
+    if params[:provider] == "1"
+      search_term = params[:search_term]
+      @results = $client.user_search(search_term)
+    else
+      ## fill this in later!
+      @results = []
+    end
   end
 end
