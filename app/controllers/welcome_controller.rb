267c7dd66e53d4298957003ed779cddc80316909
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-08 09:27:14 -0800

    Removing instagram stuff from welcome view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,8 @@ class WelcomeController < ApplicationController
 
   def index
     @twitter_user = TwitterUser.new()
-    @instagram = Instagram.user_recent_media("1496836825", {:count => 3})
-    @instagram_search = Instagram.user_search('dreedles')
-    @instagram_user = Instagram.user("1496836825")
+
+
     render :index
   end
 end
