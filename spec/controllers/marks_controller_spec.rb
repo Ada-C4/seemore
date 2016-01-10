818c130257ce2a9c4d85275b122aed16e2804367
Brittany <BrittanyKohler@gmail.com>
2016-01-09 19:39:26 -0800

    Adding tests for require login logic

--- spec/controllers/marks_controller_spec.rb
+++ spec/controllers/marks_controller_spec.rb
@@ -4,17 +4,49 @@ RSpec.describe MarksController, type: :controller do
     let(:new_spy) { build(:spy) }
 
   describe "GET #search" do
-    context "twitter" do
-      it "renders the search template" do
-        get :search, provider: "twitter", username: "loganmeetsworld"
-        expect(response).to render_template :search
+    context "logged in" do
+      context "twitter" do
+        it "renders the search template" do
+          new_spy.save
+          session[:spy_id] = new_spy.id
+          get :search, provider: "twitter", username: "loganmeetsworld"
+          expect(response).to render_template :search
+        end
+      end
+
+      context "vimeo" do
+        it "renders the search page" do
+          new_spy.save
+          session[:spy_id] = new_spy.id
+          get :search, provider: "vimeo", username: "loganmeetsworld"
+          expect(response).to render_template :search
+        end
       end
     end
 
-    context "vimeo" do
-      it "renders the search page" do
-        get :search, provider: "vimeo", username: "loganmeetsworld"
-        expect(response).to render_template :search
+    context "not logged in" do
+      context "twitter" do
+        it "redirects to the home page" do
+          get :search, provider: "twitter"
+          expect(response).to redirect_to root_path
+        end
+
+        it "adds a flash error" do
+          get :search, provider: "twitter"
+          expect(flash[:error]).to_not be nil
+        end
+      end
+
+      context "vimeo" do
+        it "redirects to the home page" do
+          get :search, provider: "vimeo"
+          expect(response).to redirect_to root_path
+        end
+
+        it "adds a flash error" do
+          get :search, provider: "vimeo"
+          expect(flash[:error]).to_not be nil
+        end
       end
     end
   end
