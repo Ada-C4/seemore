3e2f0e0aa51e8ce08037d79d5a49d0a4c6302b60
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-09 19:54:28 -0800

    added create Tweets method

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,6 +4,18 @@ class Tweet < ActiveRecord::Base
   validates :text, presence: true
   validates :uri, presence:true
 
-  def create_tweets
+  def create_tweets(twitter_user)
+    # grab tweets from Twitter API
+    tweets_array = $client.user_timeline(twitter_user.twitter_id.to_i)
+    # turn each tweet into a Tweet object
+    tweets_array.each do |tweet|
+      tweet_hash = {
+        twitter_id: tweet.id,
+        text: tweet.text,
+        uri: tweet.uri,
+        twitter_user_id: twitter_user.id
+      }
+      Tweet.create(tweet_hash)
+    end
   end
 end
