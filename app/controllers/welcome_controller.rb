c5592dfb9e00f2c5437584b6ba36ea294b33b17d
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-10 13:32:02 -0800

    Made max widths for vimeo into constants in application helper

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,13 @@
 class WelcomeController < ApplicationController
   before_action :current_user
+  include ApplicationHelper
 
   def index
     @twitter_user = TwitterUser.new()
     @vid =
-    HTTParty.get( "https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/102805992", headers: {"Authorization" => "bearer #{vimeo_access_token}", 'Accept' => 'application/json' }, format: :json).parsed_response
+    HTTParty.get( "https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/102805992&maxwidth=#{VIMEO_MAX_WIDTH}&maxheight=#{VIMEO_MAX_HEIGHT}", headers: {"Authorization" => "bearer #{vimeo_access_token}", 'Accept' => 'application/json' }, format: :json).parsed_response
   end
+
+
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
 
-
+  VIMEO_MAX_WIDTH = "350px"
+  VIMEO_MAX_HEIGHT = "200px"
 
 end
