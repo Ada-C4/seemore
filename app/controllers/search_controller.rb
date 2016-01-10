1b7a826e4a93b89a035cb7cd8fec777d96625ca7
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-09 16:36:34 -0800

    Search controller in progress

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,11 @@
 class SearchController < ApplicationController
   def index
-    if params[:]
-    search_term = params[:search_term]
-    @results = $client.user_search(search_term)
-
+      if params[:provider] == "1"
+        search_term = params[:search_term]
+        @results = $client.user_search(search_term)
+      else
+        @results = 
+      end
+    end
   end
 end
