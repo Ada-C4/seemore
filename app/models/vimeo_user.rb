4a3eec1af5652557f85c589418d0ecccd97b321b
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-09 16:20:23 -0800

    Adding validations to vimeouser model

--- app/models/vimeo_user.rb
+++ app/models/vimeo_user.rb
@@ -1,2 +1,6 @@
 class VimeoUser < ActiveRecord::Base
+  has_and_belongs_to_many :users
+  validates :vimeo_id, presence: true
+  validates :name, presence: true
+  validates :uri, presence: true
 end
