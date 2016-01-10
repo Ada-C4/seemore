79ec305139812dee95cd8d76cd79e25c24980608
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-09 17:19:55 -0800

    Can search for vimeo users

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,12 @@
 class SearchController < ApplicationController
+  include SearchHelper
   def index
     #add logic for if there are no results returned
-    if params[:provider] == "1" #twitter
-      search_term = params[:search_term]
+    search_term = params[:search_term]
+    if twitter_result?
       @results = $client.user_search(search_term)
-    else #vimeo
-      @results = search_vimeo(search_term)
+    elsif vimeo_result?
+      @results = search_vimeo(search_term)["data"]
     end
 
   end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,11 @@
 module SearchHelper
+
+  def twitter_result?
+    params[:provider] == "1"
+  end
+
+  def vimeo_result?
+    params[:provider] == "2"
+  end
+
 end
