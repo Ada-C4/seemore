1cede9335022c4ec60a50d932d2a294e5fdcd60d
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-11 15:47:29 -0800

    Adding validations for embed and provider_created_at for videos and tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,5 +2,7 @@ class Tweet < ActiveRecord::Base
   belongs_to :twitter_users
   validates :twitter_id, presence: true
   validates :text, presence: true
-  validates :uri, presence:true
+  validates :uri, presence: true
+  validates :twitter_created_at, presence: true
+  validate :embed, presence: true
 end
