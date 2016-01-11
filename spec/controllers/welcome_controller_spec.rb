146f54dd366883edf05bba85430e1c07d64ac2e3
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-11 11:20:40 -0800

    Writing spec for welcome index redirect

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-
+  describe "GET #index" do
+    it "redirects to login route when there is no current user" do
+      session[:id] = nil
+      get :index
+      expect(subject).to redirect_to login_path
+    end
+  end
 end
