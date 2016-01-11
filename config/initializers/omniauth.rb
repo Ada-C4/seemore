05d92ac1e5be5d628bc6f884a8d8553e7ef43d66
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-11 11:10:53 -0800

    Able to login through vimeo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #     :fields => [:uid, :name, :provider, :image],
   #     :uid_field => :uid
   # else
-    # provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
+    provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
     provider :vimeo, ENV["VIMEO_CLIENT_ID"], ENV["VIMEO_CLIENT_SECRETS"]
   # end
 end
