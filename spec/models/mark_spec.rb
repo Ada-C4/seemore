318cd9045548f0cb5b8c6458c504313395149d37
Logan McDonald <loganmcdona11@gmail.com>
2016-01-12 14:37:48 -0800

    TEAMWORK SAVED THE DAY

--- spec/models/mark_spec.rb
+++ spec/models/mark_spec.rb
@@ -91,37 +91,4 @@ RSpec.describe Mark, type: :model do
       end
     end
   end
-
-  describe "#self.video_lookup" do
-    context "users videos become Media instances" do
-      it "returns an array of videos" do
-        create(:mark)
-        expect(Mark.video_lookup("jeffdesom")).to be_an Array
-      end
-      
-      it "returns an array of videos less than or equal to 10" do
-        create(:mark)
-        array = Mark.video_lookup("johnmervin")
-        expect(array.count).to be > 11
-      end
-
-      it "returns instances of Media" do
-        create(:mark)
-        array = Mark.video_lookup("jeffdesom")
-        expect(array[0]).to be_an_instance_of Medium
-      end
-
-      it "returns videos with all information" do
-        create(:mark)
-        array = Mark.video_lookup("jeffdesom")
-        expect(array[0].link).to eq "https://vimeo.com/150264292"
-      end
-
-      it "finds the video in the database rather than creating a new medium" do
-        create(:mark)
-        Mark.video_lookup("jeffdesom")
-        expect{Mark.video_lookup("jeffdesom")}.to change(Medium, :count).by(0)
-      end
-    end
-  end
 end
