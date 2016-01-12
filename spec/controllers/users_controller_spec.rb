0cd5c5af174e892320d487dd985418ee87af94a8
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-12 10:37:07 -0800

    added spec for subscriptions page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+  describe "GET 'subscriptions'" do
+    it "successfully loads the subscriptions page" do
+      get :subscriptions
+      expect(subject).to render_template :subscriptions
+    end
+  end
 end
