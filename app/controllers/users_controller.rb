ab999e35fd9fe4d263bfd68cfde0f01ae5b047cc
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-12 11:20:12 -0800

    added basic information on subscriptions page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :current_user
+
   def subscriptions
+    @twitter_subscriptions = @current_user.twitter_users
+    @vimeo_subscriptions = @current_user.vimeo_users
   end
 end
