a5de6560c6ea22fea84b2082a7cd354d8f6cf4bc
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-13 15:42:23 -0800

    fixed all twitter users specs

--- spec/controllers/twitter_users_controller_spec.rb
+++ spec/controllers/twitter_users_controller_spec.rb
@@ -53,67 +53,79 @@ RSpec.describe TwitterUsersController, type: :controller do
 
 
   describe "PATCH 'subscribe'" do
-    before(:each) do
-      session[:user_id] = user.id
-      VCR.use_cassette 'twitter_response' do
-        patch :subscribe, params
-        patch :subscribe, protected_params
+    context "when subscribe action is called with params before the test" do
+      before(:each) do
+        session[:user_id] = user.id
+        VCR.use_cassette 'twitter_response' do
+          patch :subscribe, params
+        end
       end
-    end
 
-    it "does not create a new TwitterUser if the TwitterUser already exists" do
-      existing_TwitterUser
-      expect(TwitterUser.all.length).to eq 1
-      expect(response.status).to eq 302
-      expect(subject).to redirect_to :root
-    end
-
-    it "successfully creates a new TwitterUser if the TwitterUser does not exist" do
+      it "does not create a new TwitterUser if the TwitterUser already exists" do
+        existing_TwitterUser
         expect(TwitterUser.all.length).to eq 1
         expect(response.status).to eq 302
         expect(subject).to redirect_to :root
+      end
+
+      it "successfully creates a new TwitterUser if the TwitterUser does not exist" do
+          expect(TwitterUser.all.length).to eq 1
+          expect(response.status).to eq 302
+          expect(subject).to redirect_to :root
+      end
+
+      it "creates Tweets for new TwitterUsers" do
+        expect(TwitterUser.first.tweets).not_to be_empty
+        expect(response.status).to eq 302
+        expect(subject).to redirect_to :root
+      end
+
+      it "does not create Tweets if the TwitterUser already exists" do
+        existing_TwitterUser
+        expect(response.status).to eq 302
+        expect(subject).to redirect_to :root
+      end
+
+      it "associates a TwitterUser with a User" do
+        expect(User.first.twitter_users).to include(TwitterUser.first)
+        expect(subject).to redirect_to :root
+      end
     end
 
-    it "creates Tweets for new TwitterUsers" do
-      expect(TwitterUser.first.tweets).not_to be_empty
-      expect(response.status).to eq 302
-      expect(subject).to redirect_to :root
+    context "when subscribe action is called with protected params before the test" do
+      before(:each) do
+        session[:user_id] = user.id
+        VCR.use_cassette 'twitter_response' do
+          patch :subscribe, protected_params
+        end
+      end
+
+      it "will not create a TwitterUser if the Twitter account is protected" do
+        expect(flash[:error]).to eq "That user is protected; you cannot subscribe to their tweets."
+        expect(subject).to redirect_to :root
+      end
     end
 
-    it "does not create Tweets if the TwitterUser already exists" do
-      existing_TwitterUser
-      expect(response.status).to eq 302
-      expect(subject).to redirect_to :root
-    end
+    context "when subscribe action is called inside the test" do
+      before(:each) do
+        session[:user_id] = user.id
+      end
 
-# I think this spec doesn't have the code written to make it pass:
-    # it "does not create Tweets if the TwitterUser already exists" do
-    #   expect(TwitterUser.first.tweets).to be_empty
-    #   expect(response.status).to eq 302
-    #   expect(subject).to redirect_to :root
-    # end
-
-    it "associates a TwitterUser with a User" do
-      expect(User.first.twitter_users).to include(TwitterUser.first)
-      expect(subject).to redirect_to :root
-    end
-
-    it "will not create a TwitterUser if the Twitter account is protected" do
-      expect(flash[:error]).to eq "That user is protected; you cannot subscribe to their tweets."
-      expect(subject).to redirect_to :root
-    end
-
-    it "will not associate a User with a TwitterUser if they are already associated" do
-      # second_existing_TwitterUser
-      # binding.pry
-      user.twitter_users << TwitterUser.first
-      binding.pry
-      expect(user.twitter_users.length).to eq 1
-      expect(flash[:error]).to eq "You are already subscribed to this user."
-      expect(subject).to redirect_to :root
+      it "will not associate a User with a TwitterUser if they are already associated" do
+        existing_TwitterUser
+        user.twitter_users << TwitterUser.first
+        binding.pry
+        VCR.use_cassette 'twitter_response' do
+          patch :subscribe, params
+        end
+        expect(user.twitter_users.length).to eq 1
+        expect(flash[:error]).to eq "You are already subscribed to this user."
+        expect(subject).to redirect_to :root
+      end
     end
 
   end
+
 
   describe "PATCH 'unsubscribe'" do
     before(:each) do
