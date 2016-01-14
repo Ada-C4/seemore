7a68dae13617e6af19023bb62717eac9e8c561e4
Rebecca Tolmach <rmtolmach@gmail.com>
2016-01-14 14:48:33 -0800

    search controller specs passing for some reason

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe SearchController, type: :controller do
   }
 end
 
+let(:user) do
+  User.create(uid:"1234",provider:"developer",name:"Test")
+end
+
   describe "GET 'index'" do
     context "when searching for a twitter or vimeo user" do
       it "successfully loads the search results page" do
@@ -17,15 +21,15 @@ end
         before(:each) do
           session[:user_id] = user.id
           VCR.use_cassette 'twitter_response' do
-            patch :subscribe, params
+            get :index, params
           end
         end
           it "gives you data back from twitter" do
-
+            expect(response.status).to eq 200
           end
 
           it "gives you data back from vimeo" do
-
+            expect(response.status).to eq 200
           end
       end #ends api context
 
