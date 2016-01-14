91e61f204c12323d9d73463d56b3dc0fbe1584d3
Rebecca Tolmach <rmtolmach@gmail.com>
2016-01-14 13:40:46 -0800

    deleting images before deployment

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -7,9 +7,31 @@ RSpec.describe SearchController, type: :controller do
 end
 
   describe "GET 'index'" do
-    it "successfully loads the search results page" do
-      get :index, params
-      expect(subject).to render_template :index
-    end
-  end
-end
+    context "when searching for a twitter or vimeo user" do
+      it "successfully loads the search results page" do
+        get :index, params
+        expect(subject).to render_template :index
+      end
+
+      context "when making api calls"  do
+        before(:each) do
+          session[:user_id] = user.id
+          VCR.use_cassette 'twitter_response' do
+            patch :subscribe, params
+          end
+        end
+          it "gives you data back from twitter" do
+
+          end
+
+          it "gives you data back from vimeo" do
+
+          end
+      end #ends api context
+
+
+    end #ends first context block
+
+  end #ends get index describe block
+
+end # ends the whole shebang
