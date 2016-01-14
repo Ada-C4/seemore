3a8ffe4ac2a7aa548526016e5e119793e1b22a98
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-14 09:35:31 -0800

    fixed broken vimeo specs

--- spec/controllers/vimeo_users_controller_spec.rb
+++ spec/controllers/vimeo_users_controller_spec.rb
@@ -36,53 +36,64 @@ RSpec.describe VimeoUsersController, type: :controller do
 
 
   describe "PATCH 'subscribe'" do
-    before(:each) do
-      session[:user_id] = user.id
+    context "when subscribe action is called with params before the test" do
+      before(:each) do
+        session[:user_id] = user.id
+        VCR.use_cassette 'vimeo_response' do
+          patch :subscribe, params
+        end
+      end
+
+      it "successfully creates a new VimeoUser if the VimeoUser does not exist" do
+        expect(VimeoUser.all.length).to eq 1
+        expect(response.status).to eq 302
+        expect(subject).to redirect_to :root
+      end
+
+      it "creates Videos for new VimeoUsers" do
+        expect(VimeoUser.first.videos).not_to be_empty
+        expect(response.status).to eq 302
+        expect(subject).to redirect_to :root
+      end
+
+      it "associates a VimeoUser with a User" do
+        expect(User.first.vimeo_users).to include(VimeoUser.first)
+        expect(subject).to redirect_to :root
+      end
+
     end
 
-    # it "successfully creates a new VimeoUser if the VimeoUser does not exist" do
-    #   patch :subscribe, params
-    #   expect(VimeoUser.all.length).to eq 1
-    #   expect(response.status).to eq 302
-    #   expect(subject).to redirect_to :root
-    # end
+    context "when subscribe action is called with params inside the test" do
+      before(:each) do
+        session[:user_id] = user.id
+      end
 
-    it "does not create a new VimeoUser if the VimeoUser already exists" do
-      existing_VimeoUser
-      patch :subscribe, params
-      expect(VimeoUser.all.length).to eq 1
-      expect(response.status).to eq 302
-      expect(subject).to redirect_to :root
-    end
+      it "does not create a new VimeoUser if the VimeoUser already exists" do
+        existing_VimeoUser
+        patch :subscribe, params
+        expect(VimeoUser.all.length).to eq 1
+        expect(response.status).to eq 302
+        expect(subject).to redirect_to :root
+      end
 
-    # it "creates Videos for new VimeoUsers" do
-    #   patch :subscribe, params
-    #   expect(VimeoUser.first.videos).not_to be_empty
-    #   expect(response.status).to eq 302
-    #   expect(subject).to redirect_to :root
-    # end
 
-    it "does not create Videos if the VimeoUser already exists" do
-      existing_VimeoUser
-      patch :subscribe, params
-      expect(VimeoUser.first.videos).to be_empty
-      expect(response.status).to eq 302
-      expect(subject).to redirect_to :root
-    end
+      it "does not create Videos if the VimeoUser already exists" do
+        existing_VimeoUser
+        patch :subscribe, params
+        expect(VimeoUser.first.videos).to be_empty
+        expect(response.status).to eq 302
+        expect(subject).to redirect_to :root
+      end
 
-    # it "associates a VimeoUser with a User" do
-    #   patch :subscribe, params
-    #   expect(User.first.vimeo_users).to include(VimeoUser.first)
-    #   expect(subject).to redirect_to :root
-    # end
 
-    it "will not associate a User with a VimeoUser if they are already associated" do
-      existing_VimeoUser
-      user.vimeo_users << existing_VimeoUser
-      patch :subscribe, params
-      expect(user.vimeo_users.length).to eq 1
-      expect(flash[:error]).to eq "You are already subscribed to this user."
-      expect(subject).to redirect_to :root
+      it "will not associate a User with a VimeoUser if they are already associated" do
+        existing_VimeoUser
+        user.vimeo_users << existing_VimeoUser
+        patch :subscribe, params
+        expect(user.vimeo_users.length).to eq 1
+        expect(flash[:error]).to eq "You are already subscribed to this user."
+        expect(subject).to redirect_to :root
+      end
     end
 
   end
